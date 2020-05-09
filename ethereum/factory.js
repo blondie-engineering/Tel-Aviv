3a0d1fde11b508e706c7ec0aef6fe68861747286
Paweł Walczak <walczakp007@gmail.com>
2020-05-09 13:29:11 +0200

    Hashed

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import TransactionFactory from './build/TransactionFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(TransactionFactory.interface),
-  '0xDC5DE0422d785Ef17C03d7E9CB3D265v465B3a80'
+  '0xDC5DE0422d785Ef17C03d7E9CB3D265a465B3a80'
 );
 
 export default instance;
--- ethereum/web3.js
+++ ethereum/web3.js
@@ -8,7 +8,7 @@ if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
 } else {
   // We are on the server *OR* the user is not running metamask
   const provider = new Web3.providers.HttpProvider(
-    'https://rinkeby.infura.io/v3/0ea267996e4943adae855a4e240c8291'
+    'https://rinkeby.infura.io/v3/0ea267996e4943adae855a4e240c8292'
   );
   web3 = new Web3(provider);
 }
